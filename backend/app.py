39d54a3e76bac056a490aa489bc700feb3afe023
katherineggs <katherinegarcia@ufm.edu>
2022-10-02 21:24:14 -0600

    Adds mongoDB missing adder func

--- backend/app.py
+++ backend/app.py
@@ -1,91 +1,58 @@
 from flask import Flask, render_template, request, redirect, url_for
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from pymongo import MongoClient
 from search import searchPhotos
 from recs import homeRecs
 import login as login
 import json
 import os
 
-# #base de datos
-# db = SqliteDatabase('people.db')
+# base de datos
+# MongoDB uses collections of documents instead of tables of rows
+client = MongoClient('mongodb://127.0.0.1:27017') # MongoDB instance
+db = client.boardlabDB # client ^ MongoDB db called boardlabDB
+usrs = db.users # collection called users on the boardlabDB 
+tabs = db.tabs # collection called tabs on the boardlabDB 
 
-# class Users(Model):
-#     username = CharField()
-#     email = CharField()
-#     password = CharField()
-#     class Meta:
-#         database = db 
-
-# class Tablero(Model):
-#     user = ForeignKeyField(Users, backref = 'fotos')
-#     link = CharField()
-#     class Meta:
-#         database = db 
-
-# db.connect()
-
-# db.create_tables([Users,Tablero])
-#--------------------------------------------
-
+# Flask
 template_dir = os.path.dirname(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 template_dir = os.path.join(template_dir, 'BoardLabCloud')
 template_dir = os.path.join(template_dir, 'frontend')
 static = os.path.join(template_dir, 'static')
 template_dir = os.path.join(template_dir, 'templates')
-print(template_dir)
-# template_dir = "/Users/katherinegarcia/Desktop/BoardLabCloud/frontend/templates"
 app = Flask(__name__, template_folder=template_dir, static_folder= static)
+env = Environment(loader=FileSystemLoader(template_dir),
+autoescape=select_autoescape(["html"]))
 
-# app = Flask(__name__)
-
+# Routes
 # render login / index
 @app.route('/', methods =['GET', 'POST'])
 def logIn():
-    if request.method=='POST':
-        username = request.form['username']
-        password = request.form['password']
-        # ------
-        if(login.dbUsuarios['username'] == username and login.dbUsuarios['password'] == password):
-            print(login.dbUsuarios)
-            return render_template('index.html')
-    
+  if request.method=='POST':
+    username = request.form['username']
+    password = request.form['password']
+    # ------
+    correct = login.login(username, password)
+    if correct == "Correct":
+      return render_template('index.html', username = username)
     else:
-        return render_template('login.html')
-    # ----- 
-        # dbuser = Users.select().where(Users.username == username).get()
-        
-        # if(dbuser.password == password):
-        #     return render_template('index.html', username = username)
-    
-    # else:
-    #     return render_template('login.html')
+      return render_template('login.html')
+  else:
+    return render_template('login.html')
 
 #render signup
 @app.route('/signup', methods =['GET', 'POST'])
 def signup():
-    if request.method=='POST':
-        username = request.form['username']
-        email = request.form['email']
-        password = request.form['password']
+  if request.method=='POST':
+    username = request.form['username']
+    email = request.form['email']
+    password = request.form['password']
 
-        login.signup(username, email, password)
-        return render_template('login.html')
-    
-    else:
-        return render_template('signup.html')
-        
-# @app.route('/signup', methods =['GET', 'POST'])
-# def signup():
-#     if request.method=='POST':
-#         username = request.form['username']
-#         email = request.form['email']
-#         password = request.form['password']
-#         # NewUser = Users.create(username = username,email = email, password = password)
-#         # NewUser.save()
-#         return render_template('login.html')
-    
-#     else:
-#         return render_template('signup.html')
-
+    login.signup(username, email, password)
+    return render_template('login.html')
+  else:
+    return render_template('signup.html')
+      
 # render index
 @app.route('/home', methods=['GET','POST'])
 def home():
@@ -110,7 +77,6 @@ def load():
     searchTag = str(request.args.get("tag"))
     tags = searchPhotos(searchTag,loadValue)
     tags = json.dumps(tags)
-    
   return tags
 
 # render profile
@@ -119,8 +85,9 @@ def profile():
   username = request.form['profileUser']
   linklist = []
 
-  # for i in Tablero.select().join(Users).where(Users.username == username):
-  #   linklist.append(i.link)
+  for document in tabs.find():
+      print (document)
+      # linklist.append(document)
 
   return render_template('profile.html', username = username, links = linklist)
 
@@ -129,13 +96,16 @@ def profile():
 def adder():
   link = request.form["link"]
   username = request.form['adderUser']
+  # links = []
+  # links.append(request.form["link"])
+  # dbuser = usrs.find_one({"username": username})
+  # tab = {"user" : dbuser, "link" : links}
+  # tabs.save(tab)
 
-  # dbuser = Users.select().where(Users.username == username).get()
-  # newlink = Tablero.create(user = dbuser,link= link)
-  # newlink.save()
-
+  dbuser = usrs.find_one({"username": username})
+  tab = {"user" : dbuser, "link" : link}
+  tabs.insert_one(tab)
   return render_template('profile.html', username = username)
 
-
 if __name__ == "__main__":
-  app.run(debug=True)
+  app.run(debug=True, host="0.0.0.0", port=3001)
--- backend/login.py
+++ backend/login.py
@@ -1,20 +1,34 @@
-# from peewee import *
-
-#dicccionario para simular la base de datos
-dbUsuarios = {"username": "", "email": "", "password": ""}
+from passlib.hash import pbkdf2_sha256
+from app import usrs
+import uuid
 
 #Funcion para registrar un nuevo usuario
 def signup(username, email, password):
-    dbUsuarios["username"] = username
-    dbUsuarios["email"] = email
-    dbUsuarios["password"] = password
+    # Create the user 
+    user = {
+        "_id": uuid.uuid4().hex,
+        "name": username,
+        "email": email, 
+        "password": password
+    }
 
-    print(dbUsuarios)
+    # Encrypt the password
+    user['password'] = pbkdf2_sha256.encrypt(user['password'])
 
+    # Check for existing email address
+    # if usrs.find_one({ "email": user['email'] }):
+    #     return "Email address already in use"
+    # else:
+    usrs.insert_one(user)
+    print(user)
 
 #Funcion para ingresar a home
 def login(username, password):
-    if(dbUsuarios['username'] == username and dbUsuarios['password'] == password):
-        pass
-    
-
+    print(username)
+    user = usrs.find_one({"name": username})
+    print(user)
+    if user and pbkdf2_sha256.verify(password, user['password']):
+        print("Correct")
+        return "Correct"
+    print("nope")
+    return "Invalid login credentials"
